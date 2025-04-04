536f4b6ac11e2b2a859756ff4861c5bc76422887
sayishuu <smile52673@gmail.com>
2025-04-04 17:05:24 +0900

    [Feat] 기본 패턴에 들어가는 Attack 액션노드 코드구현, Update트리에 Filp기능 구현

---
+++ Assets/Scripts/Main/MainUpdateBlackBoardAction.cs
@@ -0,0 +1,35 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "MainUpdateBlackBoard", story: "[Self] [Target]", category: "Action", id: "b6f8ce9906bd6005dbedca923e31e5be")]
+public partial class MainUpdateBlackBoardAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+
+    private float _direction;
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        // Filp
+        _direction = Target.Value.transform.position.x - Self.Value.transform.position.x;
+        if (_direction > 0)
+            Self.Value.transform.localScale = new Vector3(1, 1, 1);
+        else
+            Self.Value.transform.localScale = new Vector3(-1, 1, 1);
+
+
+        // 질문 : 여기 Success로 해도 상관없나요 어차피 repeat로 계속 도니까?
+        return Status.Running;
+    }
+}
+
