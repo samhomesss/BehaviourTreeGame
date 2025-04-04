b5dbd46670f5e3dfd57122b057a138f1d72d3983
sayishuu <smile52673@gmail.com>
2025-04-04 21:18:55 +0900

    [FEAT] 항상 실패를 반환하는 액션 노드 코드

---
+++ Assets/Scripts/Main/MainAlwaysFailAction.cs
@@ -0,0 +1,17 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "AlwaysFail", story: "Always Fail", category: "Action", id: "9667ad5c3a180f3a16c398b07b687eff")]
+public partial class MainAlwaysFailAction : Action
+{
+
+    protected override Status OnStart()
+    {
+        return Status.Failure;
+    }
+}
+
