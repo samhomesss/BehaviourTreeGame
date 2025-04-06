282da8d5e4744a10b308c6a8699d2c976abb0b69
k56754665 <95170766+k56754665@users.noreply.github.com>
2025-04-06 21:46:47 +0900

    [FIX] 검기 느려지지 않게, 메인씬 땅 추가

--- Assets/Scripts/YSH/Enemy/Boss/BossSwordForce.cs
+++ Assets/Scripts/YSH/Enemy/Boss/BossSwordForce.cs
@@ -4,27 +4,22 @@ public class BossSwordForce : MonoBehaviour
 {
     PlayerStateManager _player;
     const int SWORD_FORCE_SPEED = 25;
-    Vector2 dir;
-
+    float dir;
     
     void Start()
     {
 
         _player = FindAnyObjectByType<PlayerStateManager>();
-        dir = _player.transform.position - this.transform.position;
+        dir = (_player.transform.position.x - this.transform.position.x) > 0 ? 1 : -1 ;
 
-        if (dir.x < 0)
+        if (dir < 0)
         {
             this.transform.localScale = new Vector3(-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
-
         }
-
     }
 
     void Update()
     {
-        
-        transform.Translate(new Vector3(dir.normalized.x, 0, 0) * SWORD_FORCE_SPEED * Time.deltaTime);
-        
+        transform.Translate(new Vector3(dir, 0, 0) * SWORD_FORCE_SPEED * Time.deltaTime);
     }
 }
