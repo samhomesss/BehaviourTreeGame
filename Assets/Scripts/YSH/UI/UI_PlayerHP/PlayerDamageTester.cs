b870911641c9edf9e8e3ee2702975f22a96ed232
Yoon.07.09 <159544864+samhomesss@users.noreply.github.com>
2025-04-06 23:01:55 +0900

    [FEAT] TitleScene 조정

--- Assets/Scripts/YSH/UI/UI_PlayerHP/PlayerDamageTester.cs
+++ Assets/Scripts/YSH/UI/UI_PlayerHP/PlayerDamageTester.cs
@@ -7,7 +7,7 @@ public class PlayerDamageTester : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             //Todo: 해당 부분을 보스가 플레이어 때렸을때 피격 판정에 넣으면 됨 
-            PlayerHpManger.PlayerHpDamageEvent.PlayerDamaged(14); 
+            PlayerHpManger.PlayerHpDamageEvent.PlayerDamaged(3); 
         }
     }
 }
